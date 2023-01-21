a0d3ff2e7ea99186c6c8a724415d3f6803c0e001
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-01-20 23:36:55 -0300

    ...

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ function changeSize(){
 }
 
 function clickmenu(){
+    const icone = document.getElementById('icon')
     if (links.style.display == 'block'){
+        icone.src = 'img/close.png'
         links.style.display = 'none'
     }else{
         links.style.display = 'block'
-    }
+    }
 }
+document.getElementById('icon').src = 'img/close.png';
